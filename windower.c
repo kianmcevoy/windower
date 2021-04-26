01c1db15bdcaed9225d2148996ca37205f37447c
kian <kianmcevoy@gmail.com>
2021-04-26 16:08:25 +0100

    added hann, blackman and blackman-harris functions

--- windower.c
+++ windower.c
@@ -13,7 +13,7 @@ void OutputWindow(FILE* fp, char* type, int size)
         
         for(int n = 0; n < size; n++)
 		{
-			double output = 0.54 - 0.46 * cos((2*M_PI*n) / size);
+			double output = 0.54 - 0.46 * cos((2*M_PI*n) / (size-1));
 			fprintf(fp, "\t%.8f,\n", output);
 		}
 
@@ -26,7 +26,20 @@ void OutputWindow(FILE* fp, char* type, int size)
        
         for(int n = 0; n < size; n++)
 		{
-			double output = 0.5 - 0.5 * cos((2*M_PI*n) / size);
+			double output = 0.5 - 0.5 * cos((2*M_PI*n) / (size-1));
+			fprintf(fp, "\t%.8f,\n", output);
+		}
+
+		fprintf(fp, "};\n\n");
+	}
+
+    else if (strcmp(type, "hann") == 0)
+	{
+		fprintf(fp, "static const float %s[%d] =\n{\n" , type, size);
+       
+        for(int n = 0; n < size; n++)
+		{
+			double output = 0.5 * (1 - cos((2*M_PI*n) / (size-1)));
 			fprintf(fp, "\t%.8f,\n", output);
 		}
 
@@ -38,7 +51,7 @@ void OutputWindow(FILE* fp, char* type, int size)
 		double spread = 0.125;
 		double peak = 0.5;
         double normFactor = sqrt(2*(M_PI*(spread*spread)));
-        printf("normFactor = %.8f\n", normFactor);
+
 		fprintf(fp, "static const float %s[%d] =\n{\n" , type, size);
 
         for(int n = 0; n < size; n++)
@@ -46,6 +59,34 @@ void OutputWindow(FILE* fp, char* type, int size)
 			double index = (double)n/size;
 			double output = normFactor*(exp((-1*(index-peak)*(index-peak))/(2*spread*spread))/(spread*sqrt(2*M_PI)));
  			fprintf(fp, "\t%.8f,\n", output);
+		}
+
+		fprintf(fp, "};\n\n");
+	}
+
+    else if (strcmp(type, "blackman") == 0)
+	{
+		fprintf(fp, "static const float %s[%d] =\n{\n" , type, size);
+       
+        for(int n = 0; n < size; n++)
+		{
+            double factor = (2*M_PI)/(size-1);
+			double output = 0.42 - 0.5*cos(factor*n)+0.08*cos(2*(factor*n));
+			fprintf(fp, "\t%.8f,\n", output);
+		}
+
+		fprintf(fp, "};\n\n");
+	}
+
+    else if (strcmp(type, "blackman-harris") == 0)
+	{
+		fprintf(fp, "static const float %s[%d] =\n{\n" , type, size);
+       
+        for(int n = 0; n < size; n++)
+		{
+            double factor = (2*M_PI)/(size-1);
+			double output = 0.35875 - 0.48829 * cos(factor*n) + 0.14128 * cos(2*factor*n) - 0.01168 * cos(3*factor*n);
+			fprintf(fp, "\t%.8f,\n", output);
 		}
 
 		fprintf(fp, "};\n\n");
